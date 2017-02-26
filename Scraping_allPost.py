5d2efe8fdb2ced19de924fc4a15c68bec9a5529b
RicardoMoya <RicardoMoya>
2017-02-26 10:25:41 +0100

    code style

--- Scraping_allPost.py
+++ Scraping_allPost.py
@@ -1,24 +1,20 @@
 # -*- coding: utf-8 -*-
 __author__ = 'RicardoMoya'
 
-# REQUSITOS: tener instaladas estas dos librerias
-# $ pip install beautifulsoup4
-# $ pip install requests
-
 from bs4 import BeautifulSoup
 import requests
 
-urlBase = "http://jarroba.com/"
-maxPages = 20
+URL_BASE = "http://jarroba.com/"
+MAX_PAGES = 20
 counter = 0
 
-for i in range(1,maxPages):
+for i in range(1, MAX_PAGES):
 
     # Construyo la URL
     if i > 1:
-        url = "%spage/%d/" %(urlBase,i)
+        url = "%spage/%d/" % (URL_BASE, i)
     else:
-        url = urlBase
+        url = URL_BASE
 
     # Realizamos la petición a la web
     req = requests.get(url)
@@ -30,17 +26,17 @@ for i in range(1,maxPages):
         html = BeautifulSoup(req.text)
 
         # Obtenemos todos los divs donde estan las entradas
-        entradas = html.find_all('div',{'class':'col-md-4 col-xs-12'})
+        entradas = html.find_all('div', {'class': 'col-md-4 col-xs-12'})
 
         # Recorremos todas las entradas para extraer el título, autor y fecha
         for entrada in entradas:
             counter += 1
-            titulo = entrada.find('span', {'class' : 'tituloPost'}).getText()
-            autor = entrada.find('span', {'class' : 'autor'}).getText()
-            fecha = entrada.find('span', {'class' : 'fecha'}).getText()
+            titulo = entrada.find('span', {'class': 'tituloPost'}).getText()
+            autor = entrada.find('span', {'class': 'autor'}).getText()
+            fecha = entrada.find('span', {'class': 'fecha'}).getText()
 
             # Imprimo el Título, Autor y Fecha de las entradas
-            print "%d - %s  |  %s  |  %s" %(counter,titulo,autor,fecha)
+            print "%d - %s  |  %s  |  %s" % (counter, titulo, autor, fecha)
 
     else:
         # Si ya no existe la página y me da un 400
